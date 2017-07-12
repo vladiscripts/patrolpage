0968a7d3d385aacbeb4189acef1c44df9d781515
vladislav <blagopoluchie12@gmail.com>
2017-07-12 14:56:02 +0300

    + учёт патрулирования страниц самих редиректов, а не целевых страниц на которые они перенаправляют + чистка кода

--- patrolpage.py
+++ patrolpage.py
@@ -3,35 +3,16 @@
 #
 # author: https://github.com/vladiscripts
 #
+# API's doc: https://ru.wikipedia.org/w/api.php?action=help&modules=query%2Bflagged
 import requests
-from urllib.parse import quote
 from lxml import etree
 import re
 import pywikibot
-# import vladi_commons
-
-# запрос о последней правки страницы и патрулировавшем: [[Служебная:ApiSandbox#action=query&format=json&prop=revisions&titles=Бунт+Подразделения+по+специальным+операциям&utf8=1&rvprop=timestamp|user|comment|ids|flagged&rvlimit=5]]
-# последнего патрулировавшего так не получить. после патруля могут быть непатрульные правки и автопатрулирование. надо перебирать номера правок страницы
-
-# Для тестов:
-# не патрулировано
-# t = '''<?xml version="1.0"?><api batchcomplete=""><query><normalized><n from="Янковский,_Филипп_Олегович" to="Янковский, Филипп Олегович" /></normalized>
-# 	<pages><page _idx="320695" pageid="320695" ns="0" title="Янковский, Филипп Олегович">
-# 	<flagged stable_revid="73122852" level="0" level_text="stable" pending_since="2015-09-03T19:47:39Z" /></page></pages></query></api>'''
-# # не патрулировалась
-# t = '''<?xml version="1.0"?><api batchcomplete=""><query><pages><page _idx="6641104" pageid="6641104" ns="0" title="Таинственная страсть" />				</pages></query></api>'''
-# # патрулировано
-# t = '''<?xml version="1.0"?><api batchcomplete=""><query><pages><page _idx="13317" pageid="13317" ns="0" title="Саратов">
-#         <flagged stable_revid="81687689" level="0" level_text="stable" />     </page></pages></query></api>'''
 
 
 def page_patrolled(title):
-	q_wikiApi_base = 'https://ru.wikipedia.org/w/api.php'
-	title = normalization_pagename(str(title))
-
-	url = q_wikiApi_base + '?action=query&format=xml&prop=flagged&user&utf8=1&redirects=1&titles=' + quote(title)
-	r = requests.get(url)
-
+	params = {'action': 'query', 'prop': 'flagged', 'format': 'xml', 'titles': title, }  # 'redirects': 1
+	r = requests.get(url='https://ru.wikipedia.org/w/api.php', params=params)
 	flags = etree.fromstring(r.text)
 	if len(flags.xpath("//flagged")) == 0 or len(flags.xpath("//flagged/@pending_since")) > 0:
 		print('не патрулировано: ' + title)
@@ -41,29 +22,12 @@ def page_patrolled(title):
 		return True
 
 
-def normalization_pagename(t):
-	""" Первая буква в верхний регистр, ' ' → '_' """
-	t = t.strip()
-	return t[0:1].upper() + t[1:].replace(' ', '_')
-
-
-def check_links(string):
-	global text
-	title = re.match(r'\[\[([^|]]+)(\|[^]]+)?\]\]', link).group(1)
-	if page_patrolled(title):
-		text = text.replace(link, '<s>' + link + '</s>')
-
-
-# wikipages_filename = r'..\temp\AWBfile.txt'
-# text = vladi_commons.file_readtext(wikipages_filename)
 exclude_namespaces = r'(Special|Служебная|Участник|User|У|Обсуждение[ _]участника|ОУ|Википедия|ВП|Обсуждение[ _]Википедии|Обсуждение):'
 close_tpls = re.compile(r'\{\{([Оо]тпатрулировано|[Сс]делано|[Dd]one|[Оо]тклонено)\s*(?:\|.*?)?\}\}')
 sections_re = re.compile(r'\n={2,}[^=]+={2,}\n.*?(?=\n={2,}[^=]+={2,}\n|$)', re.DOTALL)
 link_re = re.compile(r'\s*(?<!<s>)\s*(\[\[(?!%s).*?\]\])' % exclude_namespaces)
-link_title_re = re.compile(r'\[\[([^]|]+).*?\]\]')
+linkTitle_re = re.compile(r'\[\[([^]|]+).*?\]\]')
 link_just_re = re.compile(r'\s*(\[\[(?!%s).*?\]\])' % exclude_namespaces)
-tag_li_re = re.compile(r'^[*#](.*)$', re.MULTILINE)
-header_re = re.compile(r'^==+([^=]+)==+$', re.MULTILINE)
 textend = re.compile(r'\n*$')
 
 site = pywikibot.Site('ru', 'wikipedia')
@@ -72,43 +36,30 @@ for workpage in workpages:
 	is_patrolled = False
 	is_autoclosing = False
 	page = pywikibot.Page(site, workpage)
-	text = page.get()
+	textPage = page.get()
 
-	for section in sections_re.findall(text):
+	for section in sections_re.findall(textPage):
 		if not close_tpls.search(section) and link_just_re.search(section):
-			section_workcopy = section
+			section_original = section
 
-			links_sections = link_re.findall(section_workcopy)
-			links_sections = [x for x in links_sections if x]  # чистка от пустых строк
+			links_sections = link_re.findall(section)
 			for link in links_sections:
-				link = [x for x in link if x]  # чистка от пустых строк
 
-				title = link_title_re.match(link[0]).group(1)
+				title = linkTitle_re.match(link[0]).group(1)
 				if page_patrolled(title):
-					section_workcopy = section_workcopy.replace(link[0], '<s>' + link[0] + '</s>')
+					section = section.replace(link[0], '<s>%s</s>' % link[0])
 					is_patrolled = True
 
-				# # тэги li
-				# for string in tag_li_re.findall(link[0]):
-				# 	check_links(link[0])
-				#
-				# # заголовки
-				# for string in header_re.findall(link[0]):
-				# 	check_links(link[0])
-
-			# закрытие разделов
-			links_sections = link_re.findall(section_workcopy)
-			links_sections = [x for x in links_sections if x]  # чистка от пустых строк
+			# закрытие разделов со сделаными запросами
+			links_sections = link_re.findall(section)
 			if not len(links_sections):
 				is_autoclosing = True
-				# print('автоитог в секции: ' + header_re.search(section_workcopy).group(1))
-				section_workcopy = textend.sub('\n: {{отпатрулировано}} участниками. --~~~~\n', section_workcopy)
+				section = textend.sub('\n: {{отпатрулировано}} участниками. --~~~~\n', section)
 
-			text = text.replace(section, section_workcopy)
+			textPage = textPage.replace(section_original, section)
 
-	# Запись страниц
-	# vladi_commons.file_savetext(wikipages_filename, text)
+	# Пост страницы
 	if is_patrolled or is_autoclosing:
-		page.text = text
-		edit_comment = 'зачеркнуто отпатрулированное, автоитог' if is_autoclosing else 'зачеркнуто отпатрулированное'
-		page.save(edit_comment)
+		page.text = textPage
+		summary = 'зачеркнуто отпатрулированное, автоитог' if is_autoclosing else 'зачеркнуто отпатрулированное'
+		page.save(summary)

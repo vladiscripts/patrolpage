007a201e891ee16f148d800fe2df3a64722c9381
vladiscripts <blagopoluchie12@gmail.com>
2018-03-10 10:44:11 +0300

    Fixed posting of changed pages. Code style: renamed vars and added comments

--- patrolpage.py
+++ patrolpage.py
@@ -7,7 +7,6 @@
 import requests
 import re
 import pywikibot
-# from urllib.parse import quote
 
 
 def pageInfoFromAPI(title):
@@ -29,40 +28,46 @@ def pagePatrolled(p):
 
 if __name__ == '__main__':
 	namespacesExcluded = r'(Special|Служебная|Участник|User|У|Обсуждение[ _]участника|ОУ|Википедия|ВП|Обсуждение[ _]Википедии|Обсуждение):'
-	close_tpls = re.compile(r'\{\{([Оо]тпатрулировано|[Сс]делано|[Dd]one|[Оо]тклонено)\s*(?:\|.*?)?\}\}')
+	closing_tpls = re.compile(r'\{\{([Оо]тпатрулировано|[Сс]делано|[Dd]one|[Оо]тклонено)\s*(?:\|.*?)?\}\}')
 	sections_re = re.compile(r'\n={2,}[^=]+={2,}\n.*?(?=\n={2,}[^=]+={2,}\n|$)', re.DOTALL)
 	linkNotStriked_re = re.compile(r'\s*(?<!<s>)\s*(\[\[(?!%s).*?\]\])' % namespacesExcluded)
 	linkTitle_re = re.compile(r'\[\[([^]|]+).*?\]\]')
-	linkJust_re = re.compile(r'\s*(\[\[(?!%s).*?\]\])' % namespacesExcluded)
+	link_re = re.compile(r'\s*(\[\[(?!%s).*?\]\])' % namespacesExcluded)
 	textEnd = re.compile(r'\n*$')
 
 	# Параметр "user" нужен при наличии разноименных ботов на одном аккаунте tool.wmflab.org
 	# В другом случае лучше его удалить
 	site = pywikibot.Site('ru', 'wikipedia', user='TextworkerBot')
+
 	workpages = ['Википедия:Запросы к патрулирующим',
 				 'Википедия:Запросы к патрулирующим от автоподтверждённых участников']
 	for workpage in workpages:
-		is_patrolled = False
-		is_autoclosing = False
+		found_page_patrolled = False
+		was_closed_section = False
 		page = pywikibot.Page(site, workpage)
 		textPage = page.get()
 
+		# Проверка разделов
 		for section in sections_re.findall(textPage):
-			if not close_tpls.search(section) and linkJust_re.search(section):
+			if link_re.search(section) and not closing_tpls.search(section):
 				section_original = section
 				redirectsFound = set()
 
+				# Проверка ссылок в разделе
 				links_sections = linkNotStriked_re.findall(section)
 				for link in links_sections:
 					title = linkTitle_re.match(link[0]).group(1)
 					pageProperties = pageInfoFromAPI(title)
 					is_patrolled = pagePatrolled(pageProperties)
 					if is_patrolled:
+						# ссылка отптрулирована
 						section = section.replace(link[0], '<s>%s</s>' % link[0])
+						found_page_patrolled = True
 					if 'redirect' in pageProperties:
+						# ссылка является перенаправлением
 						redirectsFound.add(title)
 
-				# закрытие разделов со сделаными запросами
+				# Закрытие разделов с отработанными запросами
 				links_sections = linkNotStriked_re.findall(section)
 				if not len(links_sections):
 					if len(redirectsFound) == 0:
@@ -72,12 +77,12 @@ if __name__ == '__main__':
 							'\n: {{отпатрулировано}} участниками. В запросе были перенаправления: %s. --~~~~\n' % \
 							', '.join(['[[%s]]' % t for t in redirectsFound]),
 							section)
-					is_autoclosing = True
+					was_closed_section = True
 
 				textPage = textPage.replace(section_original, section)
 
-		# Пост страницы
-		if is_patrolled or is_autoclosing:
+		# Постинг
+		if found_page_patrolled or was_closed_section:
 			page.text = textPage
-			summary = 'зачеркнуто отпатрулированное, автоитог' if is_autoclosing else 'зачеркнуто отпатрулированное'
+			summary = 'зачеркнуто отпатрулированное, автоитог' if was_closed_section else 'зачеркнуто отпатрулированное'
 			page.save(summary)

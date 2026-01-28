3acf604c92dcc07c82e7dfc06ad0fd88375df621
vladiscripts <blagopoluchie12@gmail.com>
2026-01-28 05:10:07 +0300

    отфильтровка пространств имён и интервик через pywikibot, вместо regexp-парсинга вики-ссылок

--- patrolpage.py
+++ patrolpage.py
@@ -2,16 +2,16 @@
 # author: https://github.com/vladiscripts
 import requests
 import re
-import pywikibot
+from dataclasses import dataclass
+import pywikibot as pwb
 
 namespaces_excluded = r'(?:Special|Служебная|Участник|User|У|U|Обсуждение[ _]участника|User talk|ОУ|Википедия|ВП|Обсуждение[ _]Википедии|Обсуждение):'
-interwiki_prefix = r':?[a-z]+?:'
 closing_tpls = re.compile(r'\{\{([Оо]тпатрулировано|[Пп]атр|[Оо]тпат|[Сс]делано|[Dd]one|[Оо]тклонено)\s*(?:\|.*?)?\}\}')
 sections_re = re.compile(r'\n={2,}[^=]+={2,}\n.*?(?=\n={2,}[^=]+={2,}\n|$)', flags=re.DOTALL)
 clean_all_striked = re.compile(r'<s>.*?</s>', flags=re.DOTALL | re.I)  # Удаляем всё, что внутри <s> ... </s>
 # link_not_striked_re = re.compile(r'\s*(?<!<s>)\s*(\[\[(?!%s)(?!%s).*?\]\])' % (interwiki_prefix, namespaces_excluded), flags=re.I)  # не зачёркнутая ссылка
 link_title_re = re.compile(r'\[\[([^]|]+).*?\]\]')  # заголовок целевой страницы из ссылки
-link_re = re.compile(r'\s*(\[\[(?!%s)(?!%s).*?\]\])' % (interwiki_prefix, namespaces_excluded), flags=re.I)
+link_re = re.compile(r'\s*(\[\[(?!%s).*?\]\])' % namespaces_excluded, flags=re.I)
 
 
 class ForumPageChangedStatus:
@@ -20,20 +20,31 @@ class ForumPageChangedStatus:
         self.is_section_closed = False
 
 
+@dataclass
+class LinkData:
+    wikilink: str
+    pwb_link: pwb.Link
+    api_data: dict | None = None
+
+
 s = requests.Session()
 s.headers = {'User-Agent': '[[w:ru:User:TextworkerBot]] / page revision checker'}
 
 
-def get_pagesdata_from_api(titles: list[str]) -> dict | None:
+def get_pagesdata_from_api(links: dict[str, LinkData]) -> dict:
     """ https://ru.wikipedia.org/w/api.php?action=query&format=json&prop=flagged|info&utf8=1&titles=ЗАГЛАВИЕ  # или ЗАГЛАВИЕ1|ЗАГЛАВИЕ2 """
-    params = {'action': 'query', 'prop': 'flagged|info', 'titles': '|'.join(titles), 'format': 'json', 'utf8': 1, }  # 'redirects': 1
+    params = {'action': 'query', 'prop': 'flagged|info', 'titles': '|'.join(links.keys()), 'format': 'json', 'utf8': 1, }  # 'redirects': 1
     r = s.get(url='https://ru.wikipedia.org/w/api.php', params=params)
     j = r.json()['query'].get('pages').values()
     if not j:
-        print("нет ['query']['pages'] в " + r.request.url)
-        return None
-    pages_info = {i['title']: i for i in j}
-    return pages_info
+        print(f"нет ['query']['pages'] в {r.request.url}")
+        return {}
+    for p in j:
+        if link_data := links.get(p['title']):
+            link_data.api_data = p
+        else:
+            print(f'не найден title "{p["title"]}" в {r.request.url}')
+    return links
 
 
 def is_page_patrolled(p: dict) -> bool:
@@ -46,40 +57,36 @@ def is_page_patrolled(p: dict) -> bool:
         return True
 
 
-def title_normalize(text) -> str:
-    title = link_title_re.match(text).group(1).replace('_', ' ').strip()
-    return title[0].upper() + title[1:] if title else ''
-
-
 def get_links_not_striked(text) -> list[str]:
     text_cleaned = clean_all_striked.sub('', text)
     links = link_re.findall(text_cleaned)
     return links
 
 
+def links_to_dict_with_filter(wikilink_not_striked) -> dict[str, LinkData]:
+    links = {}
+    for wikilink in wikilink_not_striked:
+        pwb_link = pwb.Link(link_title_re.match(wikilink).group(1))
+        # фильтр пространств имён и интервик
+        if pwb_link.namespace.id not in [-1, 1, 2, 3, 4, 5] and not pwb_link._is_interwiki:
+            links[pwb_link.title] = LinkData(wikilink=wikilink, pwb_link=pwb_link)
+    return links
+
+
 def section_links_processing(d: ForumPageChangedStatus, section: str, redirects: set) -> tuple[ForumPageChangedStatus, str, set]:
     """ Проверка ссылок в разделе, зачёркивание """
     wikilink_not_striked = get_links_not_striked(section)  # не зачёркнутые викиссылки
-    if not wikilink_not_striked:
-        return d, section, redirects
-    titles = [title_normalize(link) for link in wikilink_not_striked]
-    page_properties = get_pagesdata_from_api(titles)
-    if not page_properties:
-        return d, section, redirects
-
-    for link in wikilink_not_striked:
-        title = title_normalize(link)
-        page_info = page_properties.get(title)
-        if not page_info:
-            print(f'не найден title "{title}" в page_properties')
-            continue
-        if is_page_patrolled(page_info):
-            # ссылка отпатрулирована, зачёркиваем
-            d.is_patrolled_page_found = True
-            section = section.replace(link, '<s>%s</s>' % link)
-        if 'redirect' in page_info:
-            # ссылка является перенаправлением
-            redirects.add(title)
+    if wikilink_not_striked:
+        links = links_to_dict_with_filter(wikilink_not_striked)
+        links = get_pagesdata_from_api(links)
+        for title, link in links.items():
+            if is_page_patrolled(link.api_data):
+                # ссылка отпатрулирована, зачёркиваем
+                d.is_patrolled_page_found = True
+                section = section.replace(link.wikilink, f'<s>{link.wikilink}</s>')
+            if 'redirect' in link.api_data:
+                # ссылка является перенаправлением
+                redirects.add(title)
     return d, section, redirects
 
 
@@ -102,12 +109,12 @@ def section_closing(d: ForumPageChangedStatus, section: str, redirects: set) -> 
 
 
 def main():
-    site = pywikibot.Site('ru', 'wikipedia', user='TextworkerBot')
+    site = pwb.Site('ru', 'wikipedia', user='TextworkerBot')
     workpages = ['Википедия:Запросы к патрулирующим',
                  'Википедия:Запросы к патрулирующим от автоподтверждённых участников']
     for workpage in workpages:
         # Загрузка страницы запросов
-        page = pywikibot.Page(site, workpage)
+        page = pwb.Page(site, workpage)
         page_text = page.get()
         d = ForumPageChangedStatus()
 
@@ -132,10 +139,9 @@ def main():
 def _test():
     d = ForumPageChangedStatus()
     page_text = """
-== <s>[[Когурё]]</s> ==
-
-47 правок. Статья в кошмарном состоянии с кучей ОРИСС без источников. Подтвердите пожалуйста мои правки чтобы я могла продолжить чистить от ОРИСС.  [[У:Ulianurlanova|Ulianurlanova]] ([[ОУ:Ulianurlanova|обс.]]) 02:04, 19 января 2022 (UTC)
-* {{Отказано}} Править можно и без подтверждения. А сейчас в статье куча проблем - такое не патрулируется. --[[У:EstherColeman|<span style="color:#000000;font-family:Segoe Script;">Esther Coleman</span>]] <sup>[[ОУ:EstherColeman|обс.]]</sup> 06:51, 19 января 2022 (UTC)
+== [[en:Vogue]] ==
+[[   Vogue: Глазами редактора  | werwrwrwr ]] 
+интервики — [[У:Petsernik|Petsernik]] ([[ОУ:Petsernik|обс.]]) 14:59, 72 мартобря 2326 (UTC)
 
 == [[День пожарной охраны России]] ==
 22 правки. — [[Special:Contributions/2A00:1370:8186:BB3:A30C:5EA3:D6AA:40E6|2A00:1370:8186:BB3:A30C:5EA3:D6AA:40E6]] 21:07, 30 апреля 2025 (UTC)
